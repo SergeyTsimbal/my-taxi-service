c0e3af25a157c30b8ca9d5c0865c296e5043e13b
Sergey Tsymbal <sfisher@ukr.net>
2022-10-16 00:53:09 +0300

    First commit

---
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -0,0 +1,32 @@
+package taxi.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String URL = "jdbc:mysql://sql5.freemysqlhosting.net:3306/sql5526802";
+    private static final String USERNAME = "sql5526802";
+    private static final String PASSWORD = "NUJpfUxgLa";
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't find SQL Driver", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Properties dbProperties = new Properties();
+        dbProperties.setProperty("user", USERNAME);
+        dbProperties.setProperty("password", PASSWORD);
+        try {
+            return DriverManager.getConnection(URL, dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB ", e);
+        }
+    }
+}
